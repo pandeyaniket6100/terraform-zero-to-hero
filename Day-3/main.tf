4917f766f5ad7df0f98ffc6d72f676b2c02bfd36
Prabhat Pandey <111171595+pandeyaniket6100@users.noreply.github.com>
2026-09-15 12:44:26 +0530

    Update subnet ID in EC2 instance module

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-0b6d9d3d33ba97d99" # replace this
   instance_type_value = "t3.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value = "subnet-0e31df99c6b4b9e97" # replace this
 }
